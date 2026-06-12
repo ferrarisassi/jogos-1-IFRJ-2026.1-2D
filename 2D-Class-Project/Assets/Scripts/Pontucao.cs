ca2ba9cbdd2bc1e84d41a5bc0cb23704e53dc908
Ferrari <54215984+ferrarisassi@users.noreply.github.com>
2026-06-12 16:44:22 -0300

    moeda

---
+++ 2D-Class-Project/Assets/Scripts/Pontucao.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pontucao : MonoBehaviour
+{
+    float points = 0;
+    public TextMeshProUGUI scoreText;
+
+
+    void Start()
+    {
+        scoreText.text = "0";
+    }
+    public void AddPoints()
+    {
+        points++;
+        scoreText.text = points.ToString();
+    }
+}
