1cc630b886d8fd7b0c33340cc6667f96cbf7ceb4
Ferrari <54215984+ferrarisassi@users.noreply.github.com>
2026-06-19 16:07:44 -0300

    coisas

--- 2D-Class-Project/Assets/Scripts/CoinController.cs
+++ 2D-Class-Project/Assets/Scripts/CoinController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         
         if(other.CompareTag("Player")){
             Pontucao pontucao = other.GetComponent<Pontucao>();
             pontucao.AddPoints();
+            Instantiate(coinParticle, 
+                        this.transform.position, 
+                        Quaternion.Euler(-90,0,0));
             Destroy(this.gameObject);
         }
     }
