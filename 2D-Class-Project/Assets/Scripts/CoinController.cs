3d26f6ea16f2313e001d8cd36075d9945bb82c77
Ferrari <54215984+ferrarisassi@users.noreply.github.com>
2026-06-19 09:05:20 -0300

    Coin End and Camera

--- 2D-Class-Project/Assets/Scripts/CoinController.cs
+++ 2D-Class-Project/Assets/Scripts/CoinController.cs
@@ -6,7 +6,10 @@ public class CoinController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if(other.CompareTag("Player")){
+            Pontucao pontucao = other.GetComponent<Pontucao>();
+            pontucao.AddPoints();
             Destroy(this.gameObject);
         }
     }
